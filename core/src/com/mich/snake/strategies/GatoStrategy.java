20e56b91ec53e1b326d0e6243356a23494c770f4
Patricia Ulloa <patricia.azucar.8@gmail.com>
2026-06-05 17:35:45 -0400

    Ajuste de colores para el Gato

--- core/src/com/mich/snake/strategies/GatoStrategy.java
+++ core/src/com/mich/snake/strategies/GatoStrategy.java
@@ -30,28 +30,27 @@ public class GatoStrategy implements SkinStrategy {
 
     @Override
     public Color getColorBordes() {
-        return new Color(0.76f, 0.745f, 0.745f, 1); // Bordes grises para que no desentone
+        return new Color(0.9f, 0.75f, 0.3f, 1f); // Bordes grises para que no desentone
     }
     @Override
     public Color getColorObstaculos() {
-        // Color cafe (R: 0.4, G: 0.2, B: 0.1)
-        return new Color(0.4f, 0.2f, 0.1f, 1);
+        // Color naranja
+    	return new Color(0.85f, 0.45f, 0.25f, 1f);
     }
     @Override
     public void dibujarFondo(SpriteBatch batch, int anchoPantalla, int altoPantalla, int tamCelda) {
         int celdasX = anchoPantalla / tamCelda;
         int celdasY = altoPantalla / tamCelda;
+        
+        //Color verde musgo
+        batch.setColor(1.0f, 0.95f, 0.55f, 1f); 
 
-        for (int x = 1; x < celdasX - 1; x += 2) {
-            for (int y = 1; y < celdasY - 1; y += 2) {
-                if (((x / 2) + (y / 2)) % 2 == 0) {
-                    batch.setColor(0.58f, 1f, 0.368f, 1f); // Verde claro
-                } else {
-                    batch.setColor(0.50f, 0.90f, 0.30f, 1f); // Verde oscuro
-                }
+        for (int x = 1; x < celdasX - 1; x += 4) {
+            for (int y = 1; y < celdasY - 1; y += 4) {
                 batch.draw(RecursosJuego.getInstance().texPasto, x * tamCelda, y * tamCelda, tamCelda * 4, tamCelda * 4);
             }
         }
+        
         // Restauramos el color a blanco puro para no teñir el HUD ni los personajes
         batch.setColor(com.badlogic.gdx.graphics.Color.WHITE);
     }
